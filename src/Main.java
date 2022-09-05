e268bd94052b13f1b5a5763e274ed794fb1d8846
emrErtugrul <112801732+emrErtugrul@users.noreply.github.com>
2022-09-05 20:54:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        float boy,kilo;
+        double vki;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Lütfen boyunuzu giriniz : ");
+        boy= inp.nextFloat();
+
+        System.out.println("Lütfen kilonuzu giriniz :  ");
+        kilo= inp.nextFloat();
+
+        vki=(kilo/(boy*boy));
+        System.out.println("Vücut Kitle İndeksiniz :  "+vki);
+
     }
 }
